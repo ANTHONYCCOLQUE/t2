0644b6ae03939ba6757d09206e530d9eed587497
ANTHONYCCOLQUE <104391632+ANTHONYCCOLQUE@users.noreply.github.com>
2023-12-27 21:45:12 -0500

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
 
         <div className='about-content grid'>
           <div className='about-text'>
-            <p className='fs-17'>A Books library app built in react and uses the Open Library Subjects and Search APIs</p>
+            <p className='fs-17'>Este proyecto tiene como objetivo brindar a estudiantes de diversas regiones una educación más accesible al proporcionar un fácil acceso a libros virtuales. La aplicación de biblioteca de libros está construida en React y utiliza las APIs de Open Library Subjects y Search.</p>
             <p className='fs-17'></p>
           </div>
         </div>
